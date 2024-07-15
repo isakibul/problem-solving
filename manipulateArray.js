5a68c92ad4f23758a10be915c3d1596fe956dd4d
isakibul <isakibul108@gmail.com>
2024-07-15 21:13:51 +0600

    manipulate array

---
+++ manipulateArray.js
@@ -0,0 +1,24 @@
+function processArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+
+  let temp;
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = 0; j < result.length - 1 - i; j++) {
+      if (result[j] > result[j + 1]) {
+        temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+  return result;
+}
+
+const inputArray = [5, 3, 8, 3, 1, 5, 7, 8, 2, 4];
+const result = processArray(inputArray);
+console.log(result);
