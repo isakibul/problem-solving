4ae85dfc65c2b3e8e6571c517dde866fdf56a655
isakibul <isakibul108@gmail.com>
2025-06-22 11:58:41 +0600

    add: romanToInteger

--- NumberOfStudentsUnabletoEatLunch.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number[]} students
- * @param {number[]} sandwiches
- * @return {number}
- */
-var countStudents = function (students, sandwiches) {};
-
-const result = countStudents([1, 1, 0, 0], [0, 1, 0, 1]);
-console.log(result);
